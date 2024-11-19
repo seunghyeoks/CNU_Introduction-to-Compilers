b6e7014759c1b8dd4f68c0b04ec27e6fc8b6a450
BookShelf <seunghyeoks@naver.com>
2024-11-19 11:09:30 +0900

    feat: Add base code for printlistner [Week12]

---
+++ Week12/src/tinyR3PrintListener.java
@@ -0,0 +1,204 @@
+import generated.tinyR3BaseListener;
+import generated.tinyR3Parser;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+import org.antlr.v4.runtime.tree.ParseTreeProperty;
+
+public class tinyR3PrintListener extends tinyR3BaseListener implements ParseTreeListener {
+    private static String output;
+    private int depth = 0;  // {} 레벨
+    ParseTreeProperty<String> r3Tree = new ParseTreeProperty<>();
+
+    public static String getOutput() {
+        return output;
+    }
+
+    @Override public void exitProgram(tinyR3Parser.ProgramContext ctx) {
+        StringBuilder program = new StringBuilder();
+        for (int i = 0; i < ctx.decl().size(); i++)
+            program.append(r3Tree.get(ctx.decl(i)));
+        output = program.toString();
+    }
+
+    @Override public void exitDecl(tinyR3Parser.DeclContext ctx) {
+
+    }
+
+    @Override public void exitType_spec(tinyR3Parser.Type_specContext ctx) {
+        String U32 = ctx.U32().getText();
+        r3Tree.put(ctx, U32);
+    }
+
+    // {} 마다 중첩 횟수를 계산 하기 위해 depth 를 사용
+    @Override public void enterCompound_stmt(tinyR3Parser.Compound_stmtContext ctx) {
+        depth++;
+    }
+
+    // 나갈때는 depth 줄이기
+    @Override public void exitCompound_stmt(tinyR3Parser.Compound_stmtContext ctx) {
+        StringBuilder result = new StringBuilder();
+        int local_count = ctx.local_decl().size();
+        int stmt_count = ctx.stmt().size();
+        for (int i = 0; i < local_count; i++) {
+            result.append("\t".repeat(Math.max(0, depth)));
+            result.append(r3Tree.get(ctx.local_decl(i)));
+        }
+        for (int i = 0; i < stmt_count; i++) {
+            result.append("\t".repeat(Math.max(0, depth)));
+            result.append(r3Tree.get(ctx.stmt(i)));
+        }
+
+        depth--;
+        result.append("\t".repeat(Math.max(0, depth)));
+        r3Tree.put(ctx, "{\n" + result + "}\n");
+    }
+
+    @Override public void exitLocal_decl(tinyR3Parser.Local_declContext ctx) {
+        String local_decl = "";
+
+        String dec_spec = r3Tree.get(ctx.dec_spec());
+        String id = r3Tree.get(ctx.id());
+        String val = r3Tree.get(ctx.val());
+        local_decl += dec_spec + ' ' + id;
+        if (ctx.type_spec() != null) {
+            String type_spec = r3Tree.get(ctx.type_spec());
+
+            local_decl += ":" + type_spec;
+        }
+        local_decl += " = " + val + ";\n";
+
+        r3Tree.put(ctx, local_decl);
+    }
+
+    @Override public void exitDec_spec(tinyR3Parser.Dec_specContext ctx) {
+        String let = ctx.LET().getText();
+        if (ctx.MUT() != null) {
+            String mut = ctx.MUT().getText();
+            r3Tree.put(ctx, let + ' ' + mut);
+        } else {
+            r3Tree.put(ctx, let);
+        }
+    }
+
+    @Override public void exitVal(tinyR3Parser.ValContext ctx) {
+        String result = "";
+        if(ctx.literal() != null)
+            result = r3Tree.get(ctx.literal());
+        else if(ctx.id() != null)
+            result = r3Tree.get(ctx.id());
+        r3Tree.put(ctx, result);
+    }
+
+    @Override public void exitStmt(tinyR3Parser.StmtContext ctx) {
+        String result = "";
+        if (ctx.expr_stmt() != null)
+            result = r3Tree.get(ctx.expr_stmt());
+        else if (ctx.compound_stmt() != null)
+            result = r3Tree.get(ctx.compound_stmt());
+
+        r3Tree.put(ctx, result);
+    }
+
+    @Override public void exitExpr_stmt(tinyR3Parser.Expr_stmtContext ctx) {
+        String expr = r3Tree.get(ctx.expr());
+        r3Tree.put(ctx, expr + ";\n");
+    }
+
+    @Override public void exitExpr(tinyR3Parser.ExprContext ctx) {
+        String result = "";
+        if(ctx.additive_expr() != null)
+            result = r3Tree.get(ctx.additive_expr());
+        else if (ctx.id() != null) {
+            result += r3Tree.get(ctx.id());
+            result += " = ";
+            result += r3Tree.get(ctx.expr());
+        }
+        r3Tree.put(ctx, result);
+    }
+
+    @Override public void exitAdditive_expr(tinyR3Parser.Additive_exprContext ctx) {
+        String result;
+        if(ctx.additive_expr() != null) {
+            String left = r3Tree.get(ctx.additive_expr());
+            String op = ctx.getChild(1).getText();
+            String right = r3Tree.get(ctx.multiplicative_expr());
+            result = left + " " + op + " " + right;
+        } else
+            result = r3Tree.get(ctx.multiplicative_expr());
+        r3Tree.put(ctx, result);
+    }
+
+    @Override public void exitMultiplicative_expr(tinyR3Parser.Multiplicative_exprContext ctx) {
+        String result;
+        if(ctx.multiplicative_expr() != null) {
+            String left = r3Tree.get(ctx.multiplicative_expr());
+            String op = ctx.getChild(1).getText();
+            String right = r3Tree.get(ctx.unary_expr());
+            result = left + " " + op + " " + right;
+        } else
+            result = r3Tree.get(ctx.unary_expr());
+        r3Tree.put(ctx, result);
+    }
+
+    @Override public void exitUnary_expr(tinyR3Parser.Unary_exprContext ctx) {
+        String result;
+        if(ctx.expr() != null) {
+            String op = ctx.getChild(0).getText();
+            String expr = r3Tree.get(ctx.expr());
+            result = op + expr;
+        } else
+            result = r3Tree.get(ctx.factor());
+        r3Tree.put(ctx, result);
+    }
+
+    @Override public void exitFactor(tinyR3Parser.FactorContext ctx) {
+        String result = "";
+        if(ctx.expr() != null)
+            result = "(" + r3Tree.get(ctx.expr()) + ")";
+        else if (ctx.args() != null) {
+            result += r3Tree.get(ctx.id());
+            if (ctx.getChild(1).getText().equals("!"))
+                result += "!";
+            result += '(' + r3Tree.get(ctx.args()) + ')';
+        } else {
+            if(ctx.literal() != null)
+                result = r3Tree.get(ctx.literal());
+            else
+                result = r3Tree.get(ctx.id());
+        }
+        r3Tree.put(ctx, result);
+    }
+
+    @Override public void exitReturn_stmt(tinyR3Parser.Return_stmtContext ctx) {
+        String result = "";
+        result += ctx.RETURN().getText();
+        if (ctx.expr() != null)
+            result += " " + r3Tree.get(ctx.expr());
+
+        r3Tree.put(ctx, result + ";\n");
+    }
+
+    @Override public void exitArgs(tinyR3Parser.ArgsContext ctx) {
+        int count = ctx.getChildCount();
+        if(count == 0)
+            r3Tree.put(ctx, "");
+        else {
+            StringBuilder params = new StringBuilder();
+
+            for (int i = 0; i < ctx.expr().size(); i++) {
+                params.append(r3Tree.get(ctx.expr(i)));
+                if (i < ctx.expr().size() - 1)
+                    params.append(", ");
+            }
+
+            r3Tree.put(ctx, params.toString());
+        }
+    }
+
+    @Override public void exitLiteral(tinyR3Parser.LiteralContext ctx) {
+        r3Tree.put(ctx, ctx.LITERAL().getText());
+    }
+
+    @Override public void exitId(tinyR3Parser.IdContext ctx) {
+        r3Tree.put(ctx, ctx.ID().getText());
+    }
+}
--- Week12/src/tinyR3PrintListner.java
+++
@@ -1,2 +0,0 @@
-public class tinyR3PrintListner {
-}
